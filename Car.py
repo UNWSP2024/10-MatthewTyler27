aa1ec3e9fb5642db5fdb46e23fd6e416eafdc7b7
MatthewTyler27 <mltyler@students.unwsp.edu>
2025-11-07 18:04:50 -0600

    Define Car class with acceleration and braking methods
    Implemented methods for accelerating, braking, and getting speed.

--- Car.py
+++ Car.py
@@ -1,14 +1,29 @@
-# Program # 2: Car Class
-# Write a class named Car that has the following data attributes:
+#Matthew Tyler
+#11/7/25
+#Car
 
-# __year_model (for the car's year model)
-# __make (for the make of the car)
-# __speed (for the car's current speed)
-# The Car class should have an __init__ method that accepts the car's year model and make as arguments.  These values should be assigned to the object's __year_model and __make data attributes.  It should also assign 0 to the __speed data attribute.
+class Car():
+    def __init__(self, year,make,):
+        self.year = year
+        self.make = make
+        self.speed = 0
 
-# The class should also have the following methods:
+    def accelerate(self):
+        self.speed += 5
 
-# The accelerate method should add 5 to the speed data attribute each time it it called.
-# The brake method should subtract 5 from the speed data attribute each time it is called.
-# The get_speed method should return the current speed.
-# Next, design a program that creates a Car object then calls the accelerate method five times.  After each call to the accelerate method, get the current speed of the car and display it.  The call the brake method.  After each call to the brake method, get the current speed of the car and display it.
+    def brake(self):
+        self.speed -= 5
+
+    def get_speed(self):
+     return self.speed
+         
+
+vehicle = Car(2025,"Honda",)
+print()
+for i in range(5):
+    vehicle.accelerate()
+    print(vehicle.get_speed())
+
+for i in range (5):
+    vehicle.brake()
+    print(vehicle.get_speed())
